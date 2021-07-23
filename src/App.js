175b32beb7bf789968067f316b0dbd0f84c468c1
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-22 21:40:26 -0300

    Criando as rotas com react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+
 function App() {
-  return <h1>DevFlix</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
