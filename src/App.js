4a7d6e1708acf539e740782534085ba7efbca8d6
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-24 16:55:37 -0300

    atualizando rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { SectionDescription } from "./pages/SectionDescription";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route path="/descriptions" component={SectionDescription} />
       </Switch>
     </BrowserRouter>
   );
