04f42bb81defe186729d6bbcf6b839345077d5ca
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-23 21:04:04 -0300

    Atualizando a pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,10 @@
-import { useEffect, useState } from "react";
-import { getShows } from "../../api/show";
+import { Header } from "../../components/Header";
 
 const Home = () => {
-  const [shows, setShows] = useState([]);
-
-  useEffect(() => {
-    const request = async () => {
-      const response = await getShows();
-      setShows(response);
-    };
-    request();
-  }, []);
-
   return (
-    <div>
-      {shows.map(({ id, name }) => {
-        return (
-          <div key={id}>
-            <p>{name}</p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <Header title="Bohemian Rhapsody" />
+    </>
   );
 };
 export { Home };
