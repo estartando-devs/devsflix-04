6a9e895a276611b7f135dc5bf0ba4e945d7f54b7
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-07-25 20:32:36 -0300

    implementando componente para exibir a selecao de qualidade de video e de voz

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { QualityAndVoiceSelection } from "../../components/QualityAndVoiceSelection";
 
 const Home = () => {
   return (
     <>
       <Header title="Bohemian Rhapsody" />
+      <QualityAndVoiceSelection />
     </>
   );
 };
