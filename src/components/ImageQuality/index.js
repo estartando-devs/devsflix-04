705dd21df8db329ace7cb0ccc639dc2aa70cf082
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-07-25 16:30:01 -0300

    criado o componente quality-container para exibir as opcoes para resolucao

---
+++ src/components/ImageQuality/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const ImageQuality = () => {
+  return (
+    <div className="quality-container">
+      <span className="image-resolution">720 p</span>
+    </div>
+  );
+};
