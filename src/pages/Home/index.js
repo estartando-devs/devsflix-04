5941aa4cb0ddf47051943774bcc0c350a6326fb0
AidaFig <aidafigueiredo8@gmail.com>
2021-07-26 00:17:19 -0300

    refatoração de código para melhorias de componentes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,14 @@
 import { Header } from "../../components/Header";
+import { Suggestions } from "../../components/ContainerSuggestions";
+import { SectionDescription } from "../SectionDescription";
+
 
 const Home = () => {
   return (
     <>
       <Header title="Bohemian Rhapsody" />
+      <SectionDescription />
+      <Suggestions sectionTitle="Similar films" />
     </>
   );
 };
